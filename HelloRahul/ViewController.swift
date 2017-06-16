c39acec39ae3032732514878353ab8cbfd52baee
Vikas Singh <Vikas@vikas-singh.local>
2017-06-16 17:56:16 +0530

    Initial Commit

---
+++ HelloRahul/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloRahul
+//
+//  Created by Vikas Singh on 16/06/17.
+//  Copyright © 2017 rahul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
